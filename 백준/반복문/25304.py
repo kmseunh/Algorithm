86bc9015c6c82ff61be837f49da97c844f9d7bab
kmseunh <tmdgus8779@naver.com>
2023-01-22 22:23:05 +0900

    백준: 영수증

---
+++ 백준/반복문/25304.py
@@ -0,0 +1,14 @@
+X = int(input())
+
+N = int(input())
+
+sum = 0
+
+for i in range(N):
+    A, B = map(int, input().split())
+    sum += A*B
+
+if X == sum:
+    print("Yes")
+else:
+    print("No")
