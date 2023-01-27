daac37cd6c943013f1b22862f0711640928de882
kmseunh <tmdgus8779@naver.com>
2023-01-27 23:29:59 +0900

    백준: 셀프 넘버

--- 백준/함수/4673.py
+++ 백준/함수/4673.py
@@ -2,10 +2,10 @@ numbers = set(range(1, 10001))
 new_numbers = set()
 
 for i in range(1, 10001):
-    for j in range(str(i)):
+    for j in str(i):
         i += int(j)
     new_numbers.add(i)
 
-self_num = set(numbers - new_numbers)
+self_num = sorted(numbers - new_numbers)
 for i in self_num:
     print(i)
