fe898c86bc8c645318ecaa94380ce71477e883c0
kmseunh <tmdgus8779@naver.com>
2023-01-27 23:47:21 +0900

    백준: 한수

---
+++ 백준/함수/1065.py
@@ -0,0 +1,10 @@
+n = int(input())
+hansu = 0
+
+for i in range(1, n+1):
+    nums = list(map(int, str(i)))
+    if i <= 99:
+        hansu += 1
+    elif nums[0] - nums[1] == nums[1] - nums[2]:
+        hansu += 1
+print(hansu)
