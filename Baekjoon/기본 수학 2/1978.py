ec596b2a432f2a27c9497b089177038968f890d5
kmseunh <tmdgus8779@naver.com>
2023-02-04 21:47:00 +0900

    백준: 소수 찾기

--- Baekjoon/기본 수학 2/1978.py
+++ Baekjoon/기본 수학 2/1978.py
@@ -4,9 +4,9 @@ sosu = 0
 
 for i in numbers:
     for j in range(2, i+1):
-        if i%j == 0:
+        if i % j == 0:
             if i == j:
-                sosu +=1
+                sosu += 1
             break
 
 print(sosu)
