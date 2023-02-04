0259df810d87da032fd3baf1d42a7a8c890d3f79
kmseunh <tmdgus8779@naver.com>
2023-02-04 21:39:08 +0900

    백준: 소수 찾기

---
+++ Baekjoon/기본 수학 2/1978.py
@@ -0,0 +1,12 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+sosu = 0
+
+for i in numbers:
+    for j in range(2, i+1):
+        if i%j == 0:
+            if i == j:
+                sosu +=1
+            break
+
+print(sosu)
