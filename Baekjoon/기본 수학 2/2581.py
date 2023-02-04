2515fd61e19af7eb9addbf89cf5d07f5c38b63b6
kmseunh <tmdgus8779@naver.com>
2023-02-04 21:57:36 +0900

    백준: 소수

---
+++ Baekjoon/기본 수학 2/2581.py
@@ -0,0 +1,19 @@
+m = int(input())
+n = int(input())
+sosu = []
+
+for i in range(m, n+1):
+    count = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                count += 1
+                break
+        if count == 0:
+            sosu.append(i)
+
+if len(sosu) > 0:
+    print(sum(sosu))
+    print(min(sosu))
+else:
+    print('-1')
