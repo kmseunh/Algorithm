d9d55503385e767d07ed6b90eddd44355067b565
kmseunh <tmdgus8779@naver.com>
2023-02-28 12:20:47 +0900

    백준: 바구니 순서 바꾸기

---
+++ Baekjoon/심화 1/10812.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+basket = [i+1 for i in range(n)]
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    basket = basket[:i-1]+basket[k-1:j]+basket[i-1:k-1]+basket[j:]
+for i in basket:
+    print(i, end=' ')
