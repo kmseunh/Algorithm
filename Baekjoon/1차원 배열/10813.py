af5df488f8b5610cc24ead018044c39356149b03
kmseunh <tmdgus8779@naver.com>
2023-02-28 16:45:10 +0900

    백준: 공 바꾸기

--- Baekjoon/1차원 배열/10813.py
+++ Baekjoon/1차원 배열/10813.py
@@ -1,11 +1,9 @@
 n, m = map(int, input().split())
-Basket = [i for i in range(n+1)]
+Basket = [i+1 for i in range(n)]
 
 for _ in range(m):
     i, j = map(int, input().split())
-    temp = Basket[i]
-    Basket[i] = Basket[j]
-    Basket[j] = temp
+    Basket[i-1], Basket[j-1] = Basket[j-1], Basket[i-1]
 
-for i in range(1, n+1):
-    print(Basket[i], end=" ")
+for i in Basket:
+    print(i, end=" ")
