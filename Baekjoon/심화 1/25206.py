14a356a5406cd39847e63d9ff9dd19b9a6783b85
kmseunh <tmdgus8779@naver.com>
2023-03-02 20:54:01 +0900

    백준: 너의 평점은

--- Baekjoon/심화 1/25206.py
+++ Baekjoon/심화 1/25206.py
@@ -1,5 +1,5 @@
-grade = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
-score = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+credit = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0,
+          'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0}
 
 sum = 0
 result = 0
@@ -8,6 +8,6 @@ for _ in range(20):
     b = float(b)
     if c != 'P':
         sum += b
-        result += b * score[grade.index(c)]
+        result += b * credit[c]
 
 print('%.6f' % (result / sum))
